7abbd3675ebd4d0ca2468a2c8ff1b354d96ece9b
Justin Chang <justinxchang@hotmail.com>
2018-08-22 03:25:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  let rank = document.querySelectorAll("ul.ranked-list li")
+  for(let i=0; i <rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  let parent = document.getElementById("grand-node")
+  let child = parent.children[0]
+  while(child){
+    parent = child.children[0]
+    child = parent.children[0]
+  }
+  return parent
+}  
+
+
+
+// let div1 = grandNode.children[0]
+  // let div2 = div1.children[0]
+  // let div3 = div2.children[0]
+  // let div4 = div3.children[0]
+  // return div4
